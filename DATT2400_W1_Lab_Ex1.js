1785af4fced786b0adfe16e6808d5ebdacab28b7
Rocco Ali <124732913+Rocco6745667@users.noreply.github.com>
2024-10-03 17:25:13 +0000

    commit

---
+++ DATT2400_W1_Lab_Ex1.js
@@ -0,0 +1,20 @@
+//Rocco Ali 218008847
+
+//● setup() is called once when the program starts
+function setup() {
+    // this creates the canvus 
+    createCanvas(400, 400); //width and length
+      // Turn off the draw loop.
+    noLoop();
+  
+    describe('A white half-circle on the left edge of a gray square.');
+  }
+  
+  // called directly after setup() 
+  function draw() {
+    // creates the background
+    background(120);
+    
+    //prints Rocco Ali in the console
+    print("Rocco Ali")
+  }
